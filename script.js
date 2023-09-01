749c6893609d69e6d0f6d8e0e8d3a0df69e83375
zahrahasti <hastizahra984@gmail.com>
2023-09-01 17:47:06 +0330

    add files

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 import colors from "./colors.json" assert {type:"json"};
- 
+import {downloadPalette} from "./download-img.js";
+import { copyTextColor } from "./copyText.js";
 const popularColorPalette = colors.filter((color) => color?.popular === true);
 const paletteCollection=document.querySelector(".section--new");
 const trendingPalette=document.querySelector(".section--popular")
@@ -175,47 +176,7 @@ function checkSmallButton(btn){
   }else removeSmallActiveButton(btn)
 }
 // Function to download the color palette
-function downloadPalette(coloor) {
-  // Create a new canvas element
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext('2d');
 
-  // Set canvas dimensions based on the color palette size
-  const paletteWidth = coloor.offsetWidth;
-  const paletteHeight = coloor.offsetHeight;
-  canvas.width = paletteWidth;
-  canvas.height = paletteHeight;
-
-  // Get the colors from the color palette
-  const colorBoxes = coloor.getElementsByClassName('container-color');
-
-  // Loop through the color boxes and draw them on the canvas
-  let yPos = 0;
-  for (let i = 0; i < colorBoxes.length; i++) {
-    const colorBox = colorBoxes[i];
-    const color = colorBox.style.backgroundColor;
-    const boxHeight = colorBox.offsetHeight;
-
-    // Draw the color box on the canvas
-    ctx.fillStyle = color;
-    ctx.fillRect(0, yPos, paletteWidth, boxHeight);
-
-    // Increment the y-position for the next color box
-    yPos += boxHeight;
-  }
-
-  // Convert the canvas content to a data URL
-  const dataUrl = canvas.toDataURL('image/png');
-
-  // Create a temporary anchor element
-  const link = document.createElement('a');
-  link.href = dataUrl;
-  link.download = 'color-palette.png'; // Set the desired file name
-
-  // Programmatically click the link to trigger the download
-  link.click();
-}
- 
   
 function createSamllPallete(color) {
  
@@ -344,19 +305,6 @@ containerColor.map(color=>{
 
 
 
-function copyTextColor(wrapper){
-  const copyText=[...wrapper.querySelectorAll(".btn--copy__color")];
-
-  copyText.map(copy=>{
-    copy.addEventListener("click",function(){
-      navigator.clipboard.writeText(this.textContent.slice(1))
-      this.textContent="Copied";
-     setTimeout(()=>this.textContent=this.dataset.color,400)
-    
-    })
-  })
-  
-}
 
 copyTextColor(paletteCollection);
 copyTextColor(trendingPalette);
@@ -564,8 +512,7 @@ window.addEventListener("load",function(){
    })
 })
  
-
-console.log(data);
+ 
 
 
 // // todo   static import  
