77f3c7dc2b04d3c600386a012c0790667a1f21f9
zahrahasti <hastizahra984@gmail.com>
2024-01-21 15:55:11 +0330

    delete palette folder

--- src/app/Card.svelte
+++ src/app/Card.svelte
@@ -60,10 +60,10 @@ function handleClicked(e:Event,color:color<string,number>){
       <div style="animation-delay:{(id+1)*.05}s; background-color:#{codeColor};height:{100-(10+(id)*4)*id}%;" class=" decrease-height  absolute  z-[{id+10}] w-full group flex  cursor-pointer">
        <button on:click={
            function(){
-      //  codeColorBase=codeColor
-      //  codeColor="copy text";
-      //  navigator.clipboard.writeText(String(codeColor));
-      //  setTimeout(()=>codeColor=codeColorBase,500)
+       codeColorBase=codeColor
+       codeColor="copy text";
+       navigator.clipboard.writeText(String(codeColor));
+       setTimeout(()=>codeColor=codeColorBase,500)
        }
        } type="button" class="btn btn--copy__color w-max group-hover:opacity-100  self-end opacity-0 hover:bg-[rgba(0,0,0,.2)] duration-150 rounded-t-md  px-[1rem] bg-[rgba(0,0,0,.15)]">
          #{codeColor} 
@@ -76,13 +76,12 @@ function handleClicked(e:Event,color:color<string,number>){
       <div class="flex container--btn__pallete container--main__btn justify-between w-full gap-3 items-center my-[1rem]">
         <form use:enhance action="?/addColor" method="post">
           <input type="hidden" name="hidden" id="hidden" value="{JSON.stringify(color)}">
-          <button on:click class:like={color.isLike} on:click formaction="?/addColor" type="submit" class="btn btn-like text-[1.8rem]  md:text-[1.6rem] sm:text-[1.4rem] btn-like__{color.id} btn-custom" data-id="${color.id}" data-liked="false">
+          <button   class:like={color.isLike}   formaction="?/addColor" type="submit" class="btn btn-like text-[1.8rem]  md:text-[1.6rem] sm:text-[1.4rem] btn-like__{color.id} btn-custom" data-id="${color.id}" data-liked="false">
             <span>
                <svg class="{!loading ? `block` : `hidden`} stroke-[1rem]  stroke-black text-transparent w-[2rem] h-[2rem]">
                  <use href="./icon.svg#heart3"></use></svg>
              </span>
-             <div  class="{loading ? `block` : `hidden`} w-8 h-8 rounded-full animate-spin border-2 border-b-white border-t-white border-r-transparent  border-l-transparent"></div>
-            <span id="like-count" class="like-counter">{color.likes}</span>
+             <span id="like-count" class="like-counter">{color.likes}</span>
           </button>
         </form>
         <time class="text-[1.4rem] md:text-[1.2rem] text-gray-500">{formatTimeDifference(new Date(color.timer),new Date())}</time>
