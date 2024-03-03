b581244866aa7ca49a0d23836e9013f0647273c4
zahrahasti <hastizahra984@gmail.com>
2024-03-03 21:32:54 +0330

    fix the bug:show both colors that creates gradient box

--- src/components/GradientCard.svelte
+++ src/components/GradientCard.svelte
@@ -1,27 +1,30 @@
  <script lang="ts">
      export let color:string[];
-     let codeColorBase=""
+     let codeColorBase="";
  </script>
 
 
 <div style="background:linear-gradient(to right,{color});" class="rounded-md overflow-hidden group bg-gradient-to-r from-[{color[0]}] to-[{color[1]}] relative aspect-[2/1] w-full flex justify-between items-center">
-    
-    {#each color as c}
-  <div style="background:{c};" class="text-gray-500 duration-300 group-hover:opacity-100 opacity-0 grid  ease-in-ou absolute top-0 left-0  place-content-center h-full w-1/2 z-10 cursor-pointer bg-[{color[0]}]">
-    <button on:click={
-        function(){
-    codeColorBase=c
-    c="copy text";
-    navigator.clipboard.writeText(String(c));
-    setTimeout(()=>c=codeColorBase,500)
-    }
-    } type="button" >
-      {c} 
-    </button>
-</div>
-  {/each}
-    <!-- <div style="background:{color[0]};" class="text-gray-500 duration-300 group-hover:opacity-100 opacity-0 grid  ease-in-ou absolute top-0 left-0  place-content-center h-full w-1/2 z-10 cursor-pointer bg-[{color[0]}]">{color[0]}</div>
-    <div style="background:{color[1]};" class="text-white duration-300 group-hover:opacity-100 opacity-0 grid  ease-in-ou child absolute top-0 right-0  place-content-center  h-full w-1/2 z-10 cursor-pointer bg-[{color[1]}]">{color[1]}</div> -->
+
+    <div on:click={function(){
+        codeColorBase=color[0]
+        color[0]="copy text";
+        navigator.clipboard.writeText(String(color[0]));
+        setTimeout(()=>color[0]=codeColorBase,500)
+        }
+        }  
+        style="background:{color[0]};" 
+        class="text-gray-500 duration-300 group-hover:opacity-100 opacity-0 grid  ease-in-ou absolute top-0 left-0  place-content-center h-full w-1/2 z-10 cursor-pointer bg-[{color[0]}]">{color[0]}</div>
+    <div 
+     on:click={function(){
+        codeColorBase=color[0]
+        color[0]="copy text";
+        navigator.clipboard.writeText(String(color[0]));
+        setTimeout(()=>color[0]=codeColorBase,500)
+        }
+        }
+     style="background:{color[1]};" 
+     class="text-white duration-300 group-hover:opacity-100 opacity-0 grid  ease-in-ou child absolute top-0 right-0  place-content-center  h-full w-1/2 z-10 cursor-pointer bg-[{color[1]}]">{color[1]}</div>
 </div>
   
 
