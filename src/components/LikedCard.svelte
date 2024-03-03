e2c086e08c4d549f6b14d7f9f499d98ba3b94497
zahrahasti <hastizahra984@gmail.com>
2024-03-03 23:00:45 +0330

    change button likestyle

--- src/components/LikedCard.svelte
+++ src/components/LikedCard.svelte
@@ -23,8 +23,12 @@
                 <button  on:click class:like={color.isLike} formaction="?/removeColor" on:click type="submit" class="bg-gray-200 flex items-center gap-[1rem] px-[.7rem] 
                 py-[.4rem] border-[.1rem] border-gray-200 
                 rounded-[.5rem] duration-300"  data-liked="false">
-                    <span><svg class="stroke-[1rem]  stroke-black text-transparent w-[2rem] h-[2rem]"><use href="./img/icon.svg#heart3"></use></svg></span>
-                    <span id="like-count" class="like-counter">Liked</span>
+                <svg class="text-black stroke-[1rem]  stroke-black w-[2rem] h-[2rem]">
+                   <use href="./icon.svg#heart3"></use>
+                </svg>
+                <span id="like-count" class="like-counter">
+                  Liked
+                </span>
                 </button>
               </form>
             <time class="text-[1.4rem] md:text-[1.2rem] text-gray-500"></time>
