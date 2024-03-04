2255291b3785d7cd5d55bc2c65db8bff01535d72
zahrahasti <hastizahra984@gmail.com>
2024-03-04 11:44:29 +0330

    fix the bug to fetch color

--- src/routes/[color]/+page.svelte
+++ src/routes/[color]/+page.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
     import { page } from "$app/stores";
-    import { isEqual } from "$lib";
+    import { invalidateAll ,invalidate, goto} from '$app/navigation';
     import randomcolor from "randomcolor";
     type paletteColorType={isLike:boolean,colors:string[]}[];
-  export let data;
-  const dataColor = data.colors.map(obj => ({ ...obj, isLike: false })); 
+    export let data;
+    const dataColor = data.colors.map(obj => ({ ...obj, isLike: false })); 
     import { splitArrayToSmallerArrays } from "$lib";
-	import Card from "$components/Card.svelte";
+	  import Card from "$components/Card.svelte";
     let colorsPalette:paletteColorType=[],stordColors:paletteColorType=[]
     let count=36;
+
+  function renderColor(){ 
+    console.log($page.params.color);
     let colorsSpesific=randomcolor({
     count,
-    hue: `${$page.params}`
+    hue: `${$page.params.color}`
     })
- function renderColor(){ 
   const spitedArray=splitArrayToSmallerArrays(4,colorsSpesific);
  spitedArray.map(color=>
  colorsPalette.push(
@@ -32,20 +34,22 @@
    // Calculate the current scroll position of the viewport 
    const scrollPosition = window.scrollY + window.innerHeight; 
    if (scrollPosition >= totalHeight - 10) { 
-    console.log("end of page");
      count+=12;
-     colorsSpesific=randomcolor({
-     count,
-     hue: `${$page.params}`
-    })
-    colorsPalette= renderColor()
      } 
    }} /> 
- 
+ {JSON.stringify($page.params)}
  <section   
  class="mb-[12rem] mt-10 sm:mb-0 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] h-max section--new  active-page w-full  justify-between gap-[1rem]" 
   >
-  {#each colorsPalette as color} 
+  {#each splitArrayToSmallerArrays(4,randomcolor({
+    count,
+    hue: `${$page.params.color}`
+    })).map(color=>{
+      return  {
+  isLike:false,
+  colors:color
+  }
+    }) as color} 
   <Card {color}/> 
   {/each} 
 </section>
