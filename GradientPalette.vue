79c16c6a323f8b43f84699ef01438ec03e41367c
zahrahasti <hastizahra984@gmail.com>
2025-02-16 18:16:15 +0330

    update tailwindcss version and change ui

---
+++ GradientPalette.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BaseGradientCard from '@/components/common/Cards/BaseGradientCard.vue'
+import MainWrapper from '@/components/Base/MainWrapper.vue'
+</script>
+<template>
+  <MainWrapper>
+    <BaseGradientCard />
+  </MainWrapper>
+</template>
